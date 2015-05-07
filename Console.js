958c706b0bf1cd601c6ba4fc5fd270877affa669
tusharmath <tusharmath@gmail.com>
2015-05-07 15:34:35 +0530

    release 0.0.11

--- Console.js
+++ Console.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 var _ = require('underscore');
-var Package = require('./Package.json');
+var Package = require('./package.json');
 var Commands = require('./Commands');
 var cmd = new Commands();
 var title = '\nmt-Console (version: ' + Package.version + ')';
 console.log(title);
-_.times(title.length - 1, function() {
+_.times(title.length - 1, function () {
 	process.stdout.write('=');
 });
 console.log('\n');
